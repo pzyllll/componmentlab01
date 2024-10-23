ab32e50ee21cc16dda4e8d6e114b80eb1794380b
pzy <pzyllll@outlook.com>
2024-10-23 14:27:21 +0800

    8.5
    8.5

--- main.js
+++ main.js
@@ -1,21 +1,17 @@
 // const product = 'Socks'
-const { createApp, ref } = Vue
+const { createApp, ref, computed } = Vue
 
 createApp({
    setup(){
         const product = ref('Boots');
+        const brand = ref('SE 331')
         const description = ref('These boots are made for walking and provide comfort and durability for all your adventures.');
-        const image = ref('./assets/images/socks_green.jpg')
         const link = ref('https://www.camt.cmu.ac.th/index.php/th/')
-        const inStock = ref(false);
         const inventory = ref(100);
         const onSale = ref(false); 
-        const toggleOnSale = () => {
-            onSale.value = !onSale.value;
-        };
-        const toggleInStock = () => {
-            inStock.value = !inStock.value;
-        };
+        const toggleOnSale = () => {onSale.value = !onSale.value;};
+        const toggleInStock = () => { inStock.value = !inStock.value;};
+        
         const details = ref([
                         '50% cotton',
                         '30% wool',
@@ -23,20 +19,38 @@ createApp({
                     ])
             
          const variants = ref([
-           { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-            { id: 2235, color: 'blue' , image: './assets/images/socks_blue.jpg'},
+           { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' ,quantity: 50 },
+            { id: 2235, color: 'blue' , image: './assets/images/socks_blue.jpg', quantity: 0 },        
         ]);
+        const selectedVariant = ref(0);
 
         const size = ref(['S','M','L']);
         const cart = ref(0);
         function addToCart() {cart.value +=1};
+
+            
+        const title = computed(() =>{ return brand.value + ' ' + product.value })
+
+        
+
+        const image = computed(() => {
+                        return variants.value[selectedVariant.value].image;
+                    })
+        const inStock = computed(() => { 
+                        return variants.value[selectedVariant.value].quantity;
+                    })
+
+        function updateVariant(index){selectedVariant.value = index;}
         function updateImage(variantImage){image.value = variantImage}
-            
-            
 
-
+        const saleMessage = computed(() => {
+            if (onSale.value) {
+              return `${brand.value} ${product.value} is on sale`;
+            }
+            return '';
+          });
+    
         return {
-            product,
             description,
             image,
             link,
@@ -45,14 +59,20 @@ createApp({
             onSale,
             toggleOnSale,
             details,
+            updateVariant,
             variants,
             size,
             cart,
             addToCart,
             updateImage,
             toggleInStock,
-            
+            title,     
+            selectedVariant,   
+            saleMessage,    
         };
     }
   
+
 }).mount('#app')
+
+
