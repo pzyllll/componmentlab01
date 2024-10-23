1fc4719cd1be01d35696e9077e22bc93e3b2b973
pzy <pzyllll@outlook.com>
2024-10-23 10:41:32 +0800

    7.6
    7.6

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ createApp({
         const description = ref('These boots are made for walking and provide comfort and durability for all your adventures.');
         const image = ref('./assets/images/socks_green.jpg')
         const link = ref('https://www.camt.cmu.ac.th/index.php/th/')
-        const inStock = ref(true);
+        const inStock = ref(false);
         const inventory = ref(100);
         const onSale = ref(false); 
         const toggleOnSale = () => {
